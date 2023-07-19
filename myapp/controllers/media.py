37e322021b0b152084a9789d49879c0912ee7f25
Ľubor Koka <koka.lubor87@gmail.com>
2023-07-19 22:00:36 +0200

    renamed file

---
+++ myapp/controllers/media.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse, HttpResponseBadRequest, FileResponse
+from ..models import Tracks
+from .report import FILE_PATH
+
+
+# potrebny rework na path namiesto track a team id, usetrim endpoint
+def raceImage(trackID: str):
+    try:
+        track = Tracks.objects.get(id=trackID)
+        image = bytes(track.flag)
+
+        response = HttpResponse(image, content_type="image/svg+xml")
+        response["Cache-Control"] = "public, max-age=31536000"
+        response["Content-Disposition"] = "inline"
+        return response
+
+    except Exception as e:
+        print(e)
+        return HttpResponseBadRequest()
+
+
+def reportVideo(name: str):
+    video_path = FILE_PATH + name  # Replace with the actual video path
+
+    response = FileResponse(open(video_path, "rb"))
+    response["Cache-Control"] = "public, max-age=259200"
+    return response
